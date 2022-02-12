14ceaf926681b2dea84e64ead604a736bc6f56dd
eri-Q <eri.hatanaka8@gmail.com>
2022-02-12 14:02:38 +0000

    Post-function except posting images

--- db/migrate/20220210110853_remove_name_from_users.rb
+++ db/migrate/20220210110853_remove_name_from_users.rb
@@ -5,6 +5,7 @@ class RemoveNameFromUsers < ActiveRecord::Migration[5.2]
     remove_column :users, :last_name_kana
     remove_column :users, :first_name_kana
   end
+
   def down
     add_column :users, :last_name, :string
     add_column :users, :first_name, :string
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_110853) do
+ActiveRecord::Schema.define(version: 2022_02_12_120121) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
